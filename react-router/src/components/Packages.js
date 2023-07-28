602f2078028307958bd862b757507c814a88a090
leecodexxe~ <lee4works@gmail.com>
2023-07-27 21:33:08 -0700

    use react router dom

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,18 @@
 
 
-export default function Packages() {
-
+export default function Packages(props) {
+    const displayPackages = props.packages.map((eachPackage, i) => {
+       return <li key={i}>{eachPackage}</li>
+    })
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {displayPackages}
+                </ul>
             </div>
         </div>
     )
